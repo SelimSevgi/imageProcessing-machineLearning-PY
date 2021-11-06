8c91fbecffba6737c9cec3863e46681ab0629e9c
SelimSevgi <slmsvg44@gmail.com>
2021-11-07 01:35:49 +0300

    AIvirtualKeyboard _ Project completed using libraries : -opencv -cvzone -mediapipe -pynput

---
+++ AIvirtualKeyboard/README.md
@@ -0,0 +1,24 @@
+># KURULAN PAKETLER
+>>* Python@3.7.x
+>>* cvzone@1.4.1 for hand detect
+>>* mediapipe@0.8.7 for hand detect
+>>* pynput@1.7.3 for keyboard press
+>>* opencv-python@4.5.4.58
+
+># AÇIKLAMA
+>> Mediapipe modülü hand detect edilir. Mediapipe dökümanı içerisinde bulunan el indexlerinden 8 (işaret parmağı ucu) ile 12 (orta parmak ucu) arasındaki mesafeye göre pynput kullanılarak keyboard yada mouse de basma işlemi yapılabilir.
+
+># SONUÇ
+>>## 1- Referans Noktalarının Tanıtılması
+>>![Image of Yaktocat](https://cdn.discordapp.com/attachments/766765330382848081/906665219688915024/SELIM1.png)
+>>## 2- Referans Noktası ile Harfin İşaretlenmesi
+>>![Image of Yaktocat](https://cdn.discordapp.com/attachments/766765330382848081/906665124478193685/selim1.png)
+>>## 3- Referans Noktası ile Karar Noktasının Birleşmesi Sonucu Harfin Text Kısmına Yazılması
+>>![Image of Yaktocat](https://cdn.discordapp.com/attachments/766765330382848081/906665179176116224/SELIM3.png)
+
+># GİTHUB LİNK
+>>* https://pypi.org/project/pynput/
+>>* https://pypi.org/project/opencv-python/
+>>* https://github.com/cvzone/cvzone
+>>* https://github.com/google/mediapipe
+>>* https://google.github.io/mediapipe/solutions/hands.html
--- AIvirtualKeyboard/main.py
+++ AIvirtualKeyboard/main.py
@@ -1,18 +1,79 @@
-# This is a sample Python script.
+# Kütüphaneler
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Telefon kamerası
+cap = cv2.VideoCapture("http://192.168.1.44:8080/video")
 
+# El bulmak kullanılan modül
+detector = HandDetector(detectionCon=0.8)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Harf kutuları(kareler) için kullanacağımız alfabe listesi
+# 3 liste halinde
+keys = [["Q","W","E","R","T","Y","U","I","O","P"],
+        ["A","S","D","F","G","H","J","K","L",";"],
+        ["Z","X","C","V","B","N","M",",",".","/"]]
+finalText = ""
 
+def drawALL(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + h, y + h), (255, 0, 255), cv2.FILLED)  # mor kutu
+        cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)  # beyaz harf
+    return img
 
+# Harflerin listesi
+class Button():
+    def __init__(self,pos,text,size=[85,85]):
+        self.pos= pos
+        self.size = size
+        self.text = text
+buttonList = []
 
+# Harf kutuların(karelerin) listesi
+# 3 listeyi almak için for döngüsü kullanıyoruz
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    success, img=cap.read()
+    img= detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img) # Eli bulmak için
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    img = drawALL(img, buttonList)
+
+    # Elimiz var mı yok mu kontrolü (?)
+    if lmList:
+        for button in buttonList:
+            x,y = button.pos
+            w,h = button.size
+
+            # HandDetector modülüne göre el noktaların seçimi
+            if x<lmList[5][0]<x+w and y<lmList[5][1]<y+h:
+                cv2.rectangle(img, button.pos, (x + h, y + h), (175, 0, 175), cv2.FILLED)  # koyu mor renk
+                cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255),4)
+
+                # Seçilen nokta uzaklığına göre tıklama yapma
+                l,_,_=detector.findDistance(5,8,img,draw=False)
+                print(l)
+
+                # Tıklandığı zaman
+                if l<30:
+                    cv2.rectangle(img, button.pos, (x + h, y + h), (0, 255, 0), cv2.FILLED)  # yeşil renk
+                    cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255),4)
+                    finalText += button.text
+                    sleep(0.5)
+
+    # Metin kutusu oluşturma
+    cv2.rectangle(img, (50,350), (700,450), (175,0,175), cv2.FILLED)  # koyu mor renk
+    cv2.putText(img, finalText, (60, 430), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    # q tuşuyla kameradan çıkış yapılır
+    cv2.imshow("image", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
